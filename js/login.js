381ca9f58f4cac21f58476c0c6cee9fa23691593
Kevin Couture <kcouture939@gmail.com>
2019-06-24 11:27:31 -0700

    Added TODO's in js files

---
+++ js/login.js
@@ -0,0 +1,7 @@
+// This file will handle the login HTML logic.
+
+// TODO: Form Listener required, should read in a string for character name.
+//  Either load or create new hero.
+
+// TODO: Add a function to check for existing hero in local storage by name.
+
--- js/monster.js
+++ js/monster.js
@@ -1,10 +1,19 @@
+// Monster Constructor
 function Monster(name, hp, mp, imgPath){
-    this.Name = name;
-    this.Hp = hp;
-    this.Mp = mp;
-    this.imgPath = imgPath;
+  this.Name = name;
+  this.Hp = hp;
+  this.Mp = mp;
+  this.imgPath = imgPath;
 }
 
+// TODO: Add a function to save monster data to local storage
+function monsterSave() {
 
-//TODO: add a function to calculate the damage
+}
+
+// TODO: Add a function to load monster data from local storage.
+function monsterLoad() {
+
+}
+
 
