78b9d7b13914215d4a752ecb42edb4d092d672ba
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-25 12:11:56 -0700

    check

--- js/worldHub.js
+++ js/worldHub.js
@@ -1,9 +1,10 @@
 // This file will hold logic for the worldHub HTLM page such as
 //  Event listeners, form logic, ect...
 
-// TODO: Create a listener for clicking on element that will begin combat.
+
 
 // TODO: Create a listener for clicking on element that will rest hero.
+
 
 // TODO: Create a function that displays hero data. (HTML Structure required)
 
